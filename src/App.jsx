248e14051566524ceb758f6193c5955432818fc4
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-03-06 22:04:20 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Navbarlist from './Components/Navbarlist/Navbarlist';
+
+function App() {
+  return (
+    <div className="App">
+       <Navbar />
+       <Navbarlist />
+    </div>
+  );
+}
+
+export default App;
